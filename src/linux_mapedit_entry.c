15eb9d63f4755f5e7a25e7c626a523b354d75075
FlyMandi <steidlmartinez@gmail.com>
2025-12-18 18:12:22 +0100

    load render funcs like in islescape

--- src/linux_mapedit_entry.c
+++ src/linux_mapedit_entry.c
@@ -1,5 +1,4 @@
-#include "river2D_main.h"
-#include "linux_river2Dsoftware_platform.h"
+#include "mapedit_main.h"
 
 //TODO: future renderers
 //river2D_vulkan.dll / libriver2D_vulkan.so
@@ -10,12 +9,94 @@
 //
 //TODO: read tile size, tiles and animations, save them to some format which can then be read by the game
 
+#include <dlfcn.h>
+#include <stdio.h>
+
+#ifdef DEBUG
+    #define LIBPATH "./vendor/river2D/bin/debug/"
+#else
+    #define LIBPATH "./vendor/river2D/bin/release/"
+#endif
+
 int main()
 {
-    EngineData engine = {0};
-    river2D_init(&engine);
+    char *error = 0;
+    void *software = dlopen(LIBPATH "libriver2Dsoftware.so", RTLD_NOW);
+    if(!software)
+    {
+        fprintf(stderr, "\033[31;1;7mERROR: Software renderer could not be loaded.\n");
+        fputs(dlerror(), stderr);
+        fprintf(stderr, "\033[0m\n");
+        return -1;
+    }
+    void (*river2D_init)(EngineData *engine, River2D_Image *planes);
+    *(void**)&river2D_init = dlsym(software, "river2D_init");
+    if((error = dlerror()))
+    {
+        fprintf(stderr, "\033[31;1;7mERROR: Error while loading symbol river2D_init.\n");
+        fputs(error, stderr);
+        fprintf(stderr, "\033[0m\n");
+        return -1;
+    }
 
-    while(engine.running)
+    int32_t (*river2D_shutdown)(EngineData *engine);
+    *(void**)&river2D_shutdown = dlsym(software, "river2D_shutdown");
+    if((error = dlerror()))
+    {
+        fprintf(stderr, "\033[31;1;7mERROR: Error while loading symbol river2D_shutdown.\n");
+        fputs(error, stderr);
+        fprintf(stderr, "\033[0m\n");
+        return -1;
+    }
+
+    void (*river2D_bltBuffer)(EngineData *engine);
+    *(void**)&river2D_bltBuffer = dlsym(software, "river2D_bltBuffer");
+    if((error = dlerror()))
+    {
+        fprintf(stderr, "\033[31;1;7mERROR: Error while loading symbol river2D_bltBuffer.\n");
+        fputs(error, stderr);
+        fprintf(stderr, "\033[0m\n");
+        return -1;
+    }
+
+    void (*river2D_compositeImage)(EngineData *engine, River2D_Image *image, uint8_t pictop,
+                                   uint32_t offsetDstX, uint32_t offsetDstY, uint32_t offsetSrcX,
+                                   uint32_t offsetSrcY, uint32_t cropWidth, uint32_t cropHeight);
+    *(void**)&river2D_compositeImage = dlsym(software, "river2D_compositeImage");
+    if((error = dlerror()))
+    {
+        fprintf(stderr, "\033[31;1;7mERROR: Error while loading symbol river2D_compositeImage.\n");
+        fputs(error, stderr);
+        fprintf(stderr, "\033[0m\n");
+        return -1;
+    }
+
+    void (*river2D_loadText)(EngineData *engine, River2D_Image *image, const char *text,
+                             uint8_t font, uint16_t  charsize, uint32_t spacing,
+                             uint32_t offsetY, uint32_t offsetX);
+    *(void**)&river2D_loadText = dlsym(software, "river2D_loadText");
+    if((error = dlerror()))
+    {
+        fprintf(stderr, "\033[31;1;7mERROR: Error while loading symbol river2D_loadText.\n");
+        fputs(error, stderr);
+        fprintf(stderr, "\033[0m\n");
+        return -1;
+    }
+
+    EngineData    engine = {0};
+    River2D_Image planes[RIVER2D_MAX_PLANES] = {0};
+
+    river2D_loadConfig(&engine.config);
+    engine.config.choices |= RIVER2D_CHOICE_STATIC_CANVAS_BIT;
+    river2D_init(&engine, planes);
+    mapedit_init();
+
+    bool running = true;
+
+    Atom WM_DELETE = XInternAtom(engine.display, "WM_DELETE_WINDOW", false);
+    XSetWMProtocols(engine.display, engine.window, &WM_DELETE, 1);
+
+    while(running)
     {
         while(XPending(engine.display) > 0)
         {
@@ -25,33 +106,50 @@ int main()
             {
                 case KeyPress:
                 {
-                    river2D_processControls(true, event.xkey.keycode, &engine.controls);
+                    mapedit_processControls(true, event.xkey.keycode, &engine.controls);
                     break;
                 }
                 case KeyRelease:
                 {
-                    river2D_processControls(false, event.xkey.keycode, &engine.controls);
-                    break;
-                }
-                case ConfigureNotify:
-                {
-                    //move to config file that's read on init
-                    if(!engine.config.static_canvas_enable)
-                    {
-                        river2D_resizeBackbuffer(&engine, event.xconfigure.width, event.xconfigure.height);
-                    }
+                    mapedit_processControls(false, event.xkey.keycode, &engine.controls);
                     break;
                 }
                 case ClientMessage:
                 {
-                    engine.running = false;
+                    if(event.xclient.data.l[0] == (long)WM_DELETE)
+                    {
+                        running = false;
+                    }
                     break;
                 }
-                //TODO: handle ColormapNotify?
+                case Expose:
+                {
+                    mapedit_update(&engine, river2D_compositeImage, river2D_loadText);
+                    river2D_bltBuffer(&engine);
+                    break;
+                }
+                case GraphicsExpose:
+                {
+                    mapedit_update(&engine, river2D_compositeImage, river2D_loadText);
+                    river2D_bltBuffer(&engine);
+                    break;
+                }
+                case ConfigureNotify:
+                {
+                    uint32_t newWidth  = event.xconfigure.width;
+                    uint32_t newHeight = event.xconfigure.height;
+
+                    if(newWidth != engine.config.window_width || newHeight != engine.config.window_height)
+                    {
+                        engine.config.window_width  = newWidth;
+                        engine.config.window_height = newHeight;
+                    }
+                    break;
+                }
             }
         }
-        river2D_updateEditor();
-        river2D_drawFrame(&engine);
+        mapedit_update(&engine, river2D_compositeImage, river2D_loadText);
+        river2D_bltBuffer(&engine);
     }
 
     return river2D_shutdown(&engine);
