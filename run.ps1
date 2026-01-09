4128c9e5d426ec850c4a57aa86ed54d69245aca6
FlyMandi <steidlmartinez@gmail.com>
2026-01-09 18:44:02 +0100

    add windows ./run.ps1

---
+++ run.ps1
@@ -0,0 +1,95 @@
+param
+(
+    $build = "release",
+    [switch]$compile_only = $false
+)
+
+if(-Not(Test-Path ".\obj\"))
+{
+    &mkdir .\obj\
+}
+
+if(-Not(Test-Path ".\build\"))
+{
+    &mkdir .\build\
+}
+
+if(-Not(Test-Path ".\bin\"))
+{
+    &mkdir .\bin\
+}
+
+function Get-CompiledEngine()
+{
+    if(Test-Path ".\vendor\river2D\run")
+    {
+        pushd ".\vendor\river2D\"
+        .\run.ps1 $build --compile-only
+        if(0 -ne $LASTEXITCODE)
+        {
+            exit -1
+        }
+        popd
+    }
+    else
+    {
+        Write-Host "\033[31m\nERROR: can't find river2D's run script.\033[0m"
+    }
+}
+
+function Get-Compileprep()
+{
+    Write-Host ""
+    Write-Host "\033[36mcompiling river2D...\033[0m"
+    Write-Host ""
+    premake5 ecc
+    premake5 vs2022
+}
+
+if($build -eq "debug")
+{
+    Get-CompiledEngine
+    Get-Compileprep
+    pushd ".\build\"
+    &MSBuild "river2D_mapedit.sln -p:Configuration=$build -p:Platform=windows"
+}
+else if($build -eq "debug")
+{
+    Get-CompiledEngine
+    Get-Compileprep
+    pushd ".\build\"
+    &MSBuild "river2D_mapedit.sln -p:Configuration=$build -p:Platform=windows"
+}
+else if($build -eq "asan")
+{
+    Get-CompiledEngine
+    Get-Compileprep
+    pushd ".\build\"
+    &MSBuild "river2D_mapedit.sln -p:Configuration=$build -p:Platform=windows"
+}
+else
+{
+    Write-Host "\033[31m\nERROR: invalid make config: $build.\033[0m"
+    exit -2;
+}
+
+if(0 -ne $LASTEXITCODE)
+{
+    Write-Host "\033[31m\nERROR: failed to compile.\n\033[0m"
+    exit -1
+}
+
+Write-Host "\n"
+
+if($compile_only)
+{
+    exit 0
+}
+
+popd
+
+if(0 -eq $LASTEXITCODE -and -not $compile)
+{
+    Write-Host "`nrunning $target..."
+    Invoke-Expression $target
+}
