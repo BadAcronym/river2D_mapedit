b99027daf4d477a93dd9035731f835e80945a907
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:34:04 +0100

    handle & translate mouse pointer position

--- src/mapedit_main.c
+++ src/mapedit_main.c
@@ -34,15 +34,20 @@ void mapedit_init
     river2D_loadText(engine, &engine->planes[MAPEDIT_PLANE_HUD0], " MAP EDITOR",  MAPEDIT_PLANE_FONT0, 16, 1, 0, 0);
     river2D_loadText(engine, &engine->planes[MAPEDIT_PLANE_HUD0], " NEW PROJECT", MAPEDIT_PLANE_FONT0, 16, 1, 0, 50);
     river2D_loadText(engine, &engine->planes[MAPEDIT_PLANE_HUD0], "LOAD PROJECT", MAPEDIT_PLANE_FONT0, 16, 1, 0, 74);
-    river2D_loadText(engine, &engine->planes[MAPEDIT_PLANE_HUD0], "    QUIT    ", MAPEDIT_PLANE_FONT0, 16, 1, 0, 126);
+    river2D_loadText(engine, &engine->planes[MAPEDIT_PLANE_HUD0], "    QUIT", MAPEDIT_PLANE_FONT0, 16, 1, 0, 126);
+
+    //transfer to mapedit_loadConfig at some point, when keybinds should be remappable
+    engine->controls.keycodes[MAPEDIT_KEY_LEFTM]   = Button1;
+    engine->controls.keycodes[MAPEDIT_KEY_MIDDLEM] = Button2;
+    engine->controls.keycodes[MAPEDIT_KEY_RIGHTM]  = Button3;
 }
 
 void mapedit_update
 (
     EngineData *engine,
-    void (*river2D_compositeImage)(EngineData *engine, River2D_Image *image, uint8_t pictop,
-                                   uint32_t offsetDstX, uint32_t offsetDstY, uint32_t offsetSrcX,
-                                   uint32_t offsetSrcY, uint32_t cropWidth,  uint32_t cropHeight)
+    void (*river2D_compositeImage)(EngineData *engine,  River2D_Image *image, uint8_t pictop,
+                                   uint32_t offsetDstX, uint32_t offsetDstY,  uint32_t offsetSrcX,
+                                   uint32_t offsetSrcY, uint32_t cropWidth,   uint32_t cropHeight)
 ){
     river2D_compositeImage(engine, &engine->planes[MAPEDIT_PLANE_BG0], RIVER2D_PICTOP_OVER, 0, 0, 0, 0,
                            engine->planes[MAPEDIT_PLANE_BG0].width,
@@ -55,13 +60,72 @@ void mapedit_update
                            0, 0,
                            engine->planes[MAPEDIT_PLANE_HUD0].width,
                            engine->planes[MAPEDIT_PLANE_HUD0].height);
+
+    if(engine->controls.keymap & MAPEDIT_BIT_LEFTM)
+    {
+
+    }
 }
 
-void mapedit_processControls
+// TODO: (mapedit #2) handle buttons!
+// TODO: (mapedit #3) highlight buttons when hovered
+
+void mapedit_processKeys
 (
     bool               isDown,
     uint64_t           key,
     River2D_ControlMap *controls
 ){
-    return;
+}
+
+void mapedit_processButtons
+(
+    bool               isDown,
+    uint64_t           button,
+    River2D_ControlMap *controls
+){
+    if(button == controls->keycodes[MAPEDIT_KEY_LEFTM])
+    {
+        if(isDown)
+        {
+            controls->keymap |= MAPEDIT_BIT_LEFTM;
+        }
+        else
+        {
+            controls->keymap &= ~MAPEDIT_BIT_LEFTM;
+        }
+    }
+    else if(button == controls->keycodes[MAPEDIT_KEY_RIGHTM])
+    {
+        if(isDown)
+        {
+            controls->keymap |= MAPEDIT_BIT_RIGHTM;
+        }
+        else
+        {
+            controls->keymap &= ~MAPEDIT_BIT_RIGHTM;
+        }
+    }
+    else if(button == controls->keycodes[MAPEDIT_KEY_MIDDLEM])
+    {
+        if(isDown)
+        {
+            controls->keymap |= MAPEDIT_BIT_MIDDLEM;
+        }
+        else
+        {
+            controls->keymap &= ~MAPEDIT_BIT_MIDDLEM;
+        }
+    }
+}
+
+void mapedit_processPointer
+(
+    EngineData *engine,
+    uint32_t   x,
+    uint32_t   y
+){
+    Dimensions dim = river2D_getWindowSize(engine);
+    engine->controls.pointer.x = (double)x / dim.width;
+    engine->controls.pointer.y = (double)y / dim.height;
 }
