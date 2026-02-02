491e909d6ca3a97b015b0b6537503616750a764b
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:34:04 +0100

    add EditorData struct to store global state & info

--- include/mapedit_main.h
+++ include/mapedit_main.h
@@ -19,9 +19,18 @@
 #define MAPEDIT_BIT_RIGHTM  2
 #define MAPEDIT_BIT_MIDDLEM 4
 
+typedef struct EditorData
+{
+    Rect button_new;
+    Rect button_load;
+    Rect button_quit;
+}
+EditorData;
+
 extern void mapedit_init
 (
     EngineData *engine,
+    EditorData *editor,
     void (*river2D_loadText)(EngineData *engine, River2D_Image *image, const char *text,
                              uint8_t font, uint16_t charsize, uint32_t spacing,
                              uint32_t offsetY, uint32_t offsetX)
@@ -30,6 +39,7 @@ extern void mapedit_init
 extern void mapedit_update
 (
     EngineData *engine,
+    EditorData *editor,
     void (*river2D_compositeImage)(EngineData *engine, River2D_Image *image, uint8_t pictop,
                                    uint32_t offsetDstX, uint32_t offsetDstY, uint32_t offsetSrcX,
                                    uint32_t offsetSrcY, uint32_t cropWidth,  uint32_t cropHeight)
