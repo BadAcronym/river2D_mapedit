85dec788bd85598b990d64fe599352b351fc983e
BadAcronym <terribleacronym@gmail.com>
2026-09-09 21:24:45 +0200

    fix inconsisency with X11/W32 event naming

--- src/linux_mapedit_entry.c
+++ src/linux_mapedit_entry.c
@@ -69,6 +69,15 @@ int main
                         break;
                     }
 
+                    if(event.xbutton.button == RV_MOUSE3)
+                    {
+                        event.xbutton.button = RV_MOUSE2;
+                    }
+                    else if(event.xbutton.button == RV_MOUSE2)
+                    {
+                        event.xbutton.button = RV_MOUSE3;
+                    }
+
                     meProcessButtons(&editor, &engine.controls,
                                            event.xbutton.button, true);
                     break;
